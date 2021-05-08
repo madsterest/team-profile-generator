bdaee36e19a6a011c31dfdb9ca04ee8871b9dc76
madsterest <odea.madeleine@gmail.com>
2021-05-08 17:25:14 +1000

    Finished prompt loop

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-
 let employee = [];
-newEmployee();
 //connect to objects.js (export and require)
 
 //prompt for the questions
@@ -13,39 +11,57 @@ newEmployee();
 //IDEA
 //Save prompt as a function
 //in the .then, run a2nd prompt, if they answer yes, then go back to the first promot
-function newEmployee() {
+let questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your employees name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is their id no.?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is their email adress?",
+  },
+  {
+    type: "rawlist",
+    name: "employType",
+    message: "What type of Employee would you like to add?",
+    choices: ["Employee", "Manager", "Engineer", "Intern"],
+  },
+  {
+    type: "input",
+    name: "office",
+    message: "What is their office Number?",
+    when: function (answers) {
+      return answers.employType === "Manager";
+    },
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your github username?",
+    when: function (answers) {
+      return answers.employType === "Engineer";
+    },
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What school do you go to?",
+    when: function (answers) {
+      return answers.employType === "Intern";
+    },
+  },
+];
+
+function newEmployee(questions) {
   inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is your employees name?",
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "What is their id no.?",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is their email adress?",
-      },
-      {
-        type: "rawlist",
-        name: "employType",
-        message: "What type of Employee would you like to add?",
-        choices: ["Employee", "Manager", "Engineer", "Intern"],
-      },
-      {
-        type: "input",
-        name: "office",
-        message: "What is their office Number?",
-        when: function (answers) {
-          return answers.employType === "Manager";
-        },
-      },
-    ])
+    .prompt(questions)
     .then((data) => {
       createNewObj(data);
     })
@@ -53,15 +69,17 @@ function newEmployee() {
       inquirer
         .prompt([
           {
-            type: "confirm",
+            type: "rawlist",
+            name: "anotherEmploy",
             message: "Would you like to add another emloyee?",
-            choices: ["Y", "N"],
+            choices: ["Yes", "No"],
           },
         ])
-        .then(() => {
-          if (confirmed) {
-            newEmployee();
+        .then((data) => {
+          if (data.anotherEmploy === "Yes") {
+            newEmployee(questions);
           } else {
+            //Once it has stopped running, create a function that renders the info from the employee array.
             return;
           }
         });
@@ -133,7 +151,13 @@ function createNewObj(data) {
   if (data.employType === "Employee") {
     employee.push(new Employee(data.name, data.id, data.email));
     console.log(employee);
-  } else {
-    return;
+  } else if (data.employType === "Manager") {
+    employee.push(new Manager(data.name, data.id, data.email, data.office));
+  } else if (data.employType === "Engineer") {
+    employee.push(new Engineer(data.name, data.id, data.email, data.github));
+  } else if (data.employType === "Intern") {
+    employee.push(new Intern(data.name, data.id, data.email, data.school));
   }
 }
+
+newEmployee(questions);
